5ba6601b6ce18f02bed673a9ceecf9c0aea3b52e
hellohellohell012321 <hhellohellohell012321@gmail.com>
2024-08-26 21:53:29 -0700

    Update warning.lua

--- warning.lua
+++ warning.lua
@@ -1,6 +1,6 @@
 local G2L = {};
 
-G2L["1"] = Instance.new("ScreenGui", game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"));
+G2L["1"] = Instance.new("ScreenGui", game:GetService("CoreGui"))
 G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
 
 G2L["2"] = Instance.new("Frame", G2L["1"]);
