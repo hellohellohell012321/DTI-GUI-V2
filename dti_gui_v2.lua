514b698f17b33217c25a85556e2ed982f91644ea
nora <booplayz121@gmail.com>
2024-09-19 22:03:17 -0800

    Update dti_gui_v2.lua

--- dti_gui_v2.lua
+++ dti_gui_v2.lua
@@ -1,13 +1,6 @@
 -- if you are reading this
 -- fuck you
 while wait() do
-  -- if you are reading this
--- fuck you
-while wait() do
-local message = Instance.new("Message")
-message.Text = "gl bro, i leaked the script to the dti devs"
-message.Parent = game.Workspace
-end
 local message = Instance.new("Message")
 message.Text = "gl bro, i leaked the script to the dti devs"
 message.Parent = game.Workspace
